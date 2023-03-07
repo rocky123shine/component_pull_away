28a3249c9e02f99f088d628c7149aba8e9ca9733
rockychang <798795772@qq.com>
2023-03-07 18:04:17 +0800

    Navigator2.0 测试

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:component_pull_away/dao/home_dao.dart';
+import 'package:component_pull_away/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,11 +8,13 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     var homeDao = HomeDao();
     homeDao.getBanner();
+    TestRouterDelegate routerDelegate = TestRouterDelegate();
 
     // homeDao.getaaa();
     return MaterialApp(
@@ -19,54 +22,48 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Router(routerDelegate: routerDelegate),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+MaterialPage wrapPage(Widget child) {
+  return MaterialPage(key: ValueKey(child.hashCode), child: child);
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class TestRouterDelegate extends RouterDelegate<RouterPath>
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin<RouterPath> {
+  RouterPath? path;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  List<MaterialPage> pages = [
+    wrapPage(const HomePage(
+      title: 'home',
+    ))
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return Navigator(
+      key: navigatorKey,
+      pages: pages,
+      onPopPage: (route, result) {
+        if (!route.didPop(result)) return false;
+        return true;
+      },
     );
   }
+
+  @override
+  GlobalKey<NavigatorState>? get navigatorKey => GlobalKey();
+
+  @override
+  Future<void> setNewRoutePath(RouterPath configuration) async {
+    path = configuration;
+  }
+}
+
+class RouterPath {
+  final String location;
+
+  RouterPath.home() : location = "/";
 }
